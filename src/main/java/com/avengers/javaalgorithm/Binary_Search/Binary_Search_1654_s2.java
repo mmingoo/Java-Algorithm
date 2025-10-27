c3abf2362249f2e6ad4cae64d659493bdb7bd875
mmingoo <aldrn0323@naver.com>
2025-10-27 09:34:51 +0900

    Binary_Search_1654_s2

--- src/main/java/com/avengers/javaalgorithm/Binary_Search/Binary_Search_1654_s2.java
+++ src/main/java/com/avengers/javaalgorithm/Binary_Search/Binary_Search_1654_s2.java
@@ -1,8 +1,11 @@
-package com.avengers.javaalgorithm;
-import java.util.*;
-import java.io.*;
+package com.avengers.javaalgorithm.Binary_Search;
 
-public class test {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Binary_Search_1654_s2 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
@@ -41,4 +44,5 @@ public class test {
 
         System.out.println(result);
     }
+
 }

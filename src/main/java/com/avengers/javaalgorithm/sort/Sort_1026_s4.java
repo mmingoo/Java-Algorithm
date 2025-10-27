ffa928bebbea364ebc0571399a99023eca19dfd7
mmingoo <aldrn0323@naver.com>
2025-10-28 07:56:48 +0900

    Sort_1026_s4

--- src/main/java/com/avengers/javaalgorithm/sort/Sort_1026_s4.java
+++ src/main/java/com/avengers/javaalgorithm/sort/Sort_1026_s4.java
@@ -1,8 +1,15 @@
-package com.avengers.javaalgorithm;
-import java.util.*;
-import java.io.*;
-public class test {
-    public static void main(String[] args) throws IOException  {
+package com.avengers.javaalgorithm.sort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Sort_1026_s4 {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st1 = new StringTokenizer(br.readLine());

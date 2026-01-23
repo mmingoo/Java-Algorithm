5a0f8721ad8e7326602c63a16413aa055f294fd8
mmingoo <aldrn0323@naver.com>
2026-01-23 14:31:13 +0900

    시뮬레이션, s3_1063

--- src/main/java/com/avengers/시뮬레이션/s3_1063.java
+++ src/main/java/com/avengers/시뮬레이션/s3_1063.java
@@ -1,4 +1,4 @@
-package com.avengers.javaalgorithm;
+﻿package com.avengers.시뮬레이션;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,8 +6,7 @@ import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.StringTokenizer;
 
-public class JavaAlgorithmApplication {
-
+public class s3_1063 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
